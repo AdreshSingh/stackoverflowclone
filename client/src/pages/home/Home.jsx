55bb5b204623cc5649d2003a7afbec98d4788c69
AdreshSingh <seiyaryuuguuin100@gmail.com>
2024-09-16 08:37:15 +0530

    addjusting according to the layout of home

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,5 +1,18 @@
-function Home() {
 
-    return (<div> Home </div>)
+import LeftSidebar from '../../components/leftSidebar/LeftSidebar'
+import RightSidebar from '../../components/rightSidebar/RightSidebar'
+import HomeMainbar from '../../components/homeMainbar/HomeMainbar'
+
+import '../../App.css'
+
+export const Home = ({ slideIn }) => {
+    return (
+        <div className="home-container-1">
+            <LeftSidebar slideIn={slideIn} />
+            <div className="home-container-2">
+                <HomeMainbar />
+                <RightSidebar />
+            </div>
+        </div>
+    )
 }
-export default Home
