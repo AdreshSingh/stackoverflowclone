571e9cb6b95bb1c2f0ef05f16a18b06a75cbf659
AdreshSingh <adreshsingh271527@gmail.com>
2024-10-13 07:17:51 +0530

    added more functionality in home page

--- client/src/components/homeMainbar/HomeMainbar.jsx
+++ client/src/components/homeMainbar/HomeMainbar.jsx
@@ -1,5 +1,35 @@
-function HomeMainbar() {
+import { useLocation, useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
-    return (<div> HomeMainBar </div>)
+import './HomeMainBar.css'
+import { QuestionList } from './QuestionList';
+
+function HomeMainbar() {
+    const user = 1;
+    const location = useLocation()
+
+    const questionList = null;
+    return (
+        <div className='main-bar'>
+            <div className="main-bar-header">
+                {
+
+                    location.pathname === "/" ? (<h1>Top Questions</h1>) : (<h1>All Questions</h1>)
+                }
+                <button className='ask-btn'>Ask Question</button>
+            </div>
+            <div>
+                {
+                    questionList === null ? (
+                        <h1>Loading......</h1>
+                    ) : (
+                        <>
+                            <p>{questionList?.length} questions</p>
+                            <QuestionList questionList={questionList?.data} />
+                        </>
+                    )
+                }
+            </div>
+        </div>)
 }
 export default HomeMainbar
