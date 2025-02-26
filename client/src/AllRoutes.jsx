80b780e5ff543b64fc8baccb7f43d04ce0dc57c0
Devank Singh <adreshsingh271527@gmail.com>
2025-02-26 21:53:43 +0530

    added new routes for askquestion, auth, aboutauth, question and display answer.

--- client/src/AllRoutes.jsx
+++ client/src/AllRoutes.jsx
@@ -1,11 +1,21 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/home/Home'
+import { AskQuestion } from './pages/askquestion/AskQuestion'
+import { Auth } from './pages/auth/Auth'
+import { AboutAuth } from './pages/auth/AboutAuth'
+import { Question } from './pages/question/Question'
+import { DisplayAnswer } from './pages/question/DisplayAnswer'
 
 function AllRoutes({ slideIn, handleSlideIn }) {
 
     return (
         <Routes>
             <Route path='/' element={<Home slideIn={slideIn} handleSlideIn={handleSlideIn} />} />
+            <Route path='/AskQuestion' element={<AskQuestion />} />
+            <Route path='/Auth' element={<Auth />} />
+            <Route path='/AboutAuth' element={<AboutAuth />} />
+            <Route path='/Question' element={<Question slideIn={slideIn} handleSlideIn={handleSlideIn} />} />
+            <Route path='/Question:id' element={<DisplayAnswer slideIn={slideIn} handleSlideIn={handleSlideIn} />} />
         </Routes>)
 }
 export default AllRoutes
