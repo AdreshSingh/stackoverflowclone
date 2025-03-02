2e013a3efa522b44dd8b701f0b942411ffc8ce13
Devank Singh <adreshsingh271527@gmail.com>
2025-03-02 16:10:45 +0530

    added display answer page

--- client/src/pages/question/DisplayAnswer.jsx
+++ client/src/pages/question/DisplayAnswer.jsx
@@ -1,23 +1,43 @@
-import LeftSidebar from '../../components/leftSidebar/LeftSidebar'
-import RightSidebar from '../../components/rightSidebar/RightSidebar'
-import { QuestionDetails } from '../../pages/question/QuestionDetails'
-
-import copy from 'copy-to-clipboard';
 import moment from 'moment';
 
-import upvote from '../../assets/sort-up.svg';
-import downvote from '../../assets/sort-down.svg';
-
 import './DisplayAnswer.css';
-import upvote from '../../assets/sort-up.svg';
-export function DisplayAnswer({ slideIn }) {
+import { Link } from 'react-router-dom';
+import { Avatar } from '../../components/avatar/Avatar';
+
+export function DisplayAnswer({ question, handleShare }) {
+    const user = null;
+    const handleDelete = (answerid, noofanswer) => {
+
+    }
+
     return (
-        <div className="home-container-1">
-            <LeftSidebar slideIn={slideIn} />
-            <div className="home-container-2">
-                <QuestionDetails />
-                <RightSidebar />
-            </div>
+        <div>
+            {
+                question?.answer?.map((ans) => (
+                    <div className="display-ans" key={ans._id}>
+                        <p>{question?.answerbody}</p>
+                        <div className='question-actions-user'>
+                            <div>
+                                <button type='button' onClick={handleShare}>Share</button>
+                                {
+                                    user?.result?._id === ans?.userid && (
+                                        <button type='button' onClick={() => (handleDelete(ans._id, question?.noofanswer))}>Delete</button>
+                                    )
+                                }
+                            </div>
+                            <div>
+                                <p>answered {moment(ans.answeredon).fromNow()}</p>
+                                <Link to={`/Users/${ans.userid}`} className='user-link' style={{ color: "#0086d8" }}>
+                                    <Avatar backgroundcolor={'orange'} px={'8px'} py={'5px'} borderRadius={'4px'}>
+                                        {ans.userposted.charAt(0).toUppercase()}
+                                    </Avatar>
+                                    <div>{ans.userposted}</div>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                ))
+            }
         </div>
     )
 }
