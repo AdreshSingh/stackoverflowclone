5f3acb1876dee5dd5693997e39aa29d82efb8a19
Devank Singh <adreshsingh271527@gmail.com>
2025-03-02 16:09:03 +0530

    added dummy question

--- client/src/components/homeMainbar/HomeMainbar.jsx
+++ client/src/components/homeMainbar/HomeMainbar.jsx
@@ -1,5 +1,4 @@
 import { useLocation, useNavigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
 
 import './HomeMainBar.css'
 import { QuestionList } from './QuestionList';
@@ -20,7 +19,34 @@ function HomeMainbar() {
         }
     }
 
-    const questionList = null;
+    const questionList = {
+        data: [
+            {
+                "_id": "q1",
+                "questiontitle": "How to implement a binary search algorithm in Python?",
+                "questionbody": "I am trying to implement binary search algorithm in Python but i am stuck. Can someone provide example of it.",
+                "questiontags": ["Python", "Algorithms", "Binary Search"],
+                "noofanswers": 2,
+                "upvote": ["user1", "user2"],
+                "downvote": ["user789"],
+                "userposted": "codenewbie",
+                "userid": "u12345",
+                "askedon": "2024-06-10T12:00:02",
+                "answer": [{
+                    "answerbody": "Here's a simple example of a binary search algorithm in Python",
+                    "useranswered": "algoExpert",
+                    "userid": "u67890",
+                    "answeredon": "2024-06-11T12:00:02",
+                },
+                {
+                    "answerbody": "To add to the previous answer, remember that the array needs to be sorted for binary search",
+                    "useranswered": "techGuru",
+                    "userid": "u11223",
+                    "answeredon": "2024-06-15T12:00:02",
+                }]
+            }
+        ]
+    }
     return (
         <div className='main-bar'>
             <div className="main-bar-header">
