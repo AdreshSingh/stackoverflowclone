3cab8998379779b6b1a815659dd2c92bef038814
Devank Singh <adreshsingh271527@gmail.com>
2025-03-03 07:27:32 +0530

    user model for authentication

---
+++ server/models/auth.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+    {
+        name: { type: String, requied: true },
+        email: { type: String, requied: true },
+        about: { type: String, requied: true },
+        tags: { type: [String] },
+        joinedon: { type: Date, default: Date.now }
+    }
+)
+
+export default mongoose.model("User");
