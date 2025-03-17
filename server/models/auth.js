8c21aafe91d927995446c4067ec8e19faeeeb5c5
Devank Singh <adreshsingh271527@gmail.com>
2025-03-17 09:23:47 +0530

    added mongodb schema

--- server/models/auth.js
+++ server/models/auth.js
@@ -4,10 +4,11 @@ const userSchema = mongoose.Schema(
     {
         name: { type: String, requied: true },
         email: { type: String, requied: true },
+        password: { type: String, requied: true },
         about: { type: String, requied: true },
         tags: { type: [String] },
         joinedon: { type: Date, default: Date.now }
     }
 )
 
-export default mongoose.model("User");
+export default mongoose.model("User", userSchema);
