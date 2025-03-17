e940b9d50ac65fbfec69dac2e1fea0721c3eba20
Devank Singh <adreshsingh271527@gmail.com>
2025-03-17 09:23:18 +0530

    added auth logic controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,32 +2,36 @@ import users from '../models/auth.js';
 import bcrypt from 'bcryptjs';
 
 export async function signup(req, res) {
-    const [name, email, password] = req.body;
+    console.log("signup processed")
+    const { name, email, password } = req.body;
 
     try {
         const existUser = await users.findOne({ email });
 
         if (existUser) {
-            return res.status(400).json({ message: "User already exist" })
+            console.log("User already exist")
+            return res.status(300).json({ message: "User already exist" })
+
         }
 
-        const hashPassword = await bcrypt.hash(password)
+        const hashedpassword = await bcrypt.hash(password, 12);
         const newUser = await users.create(
             {
                 name,
                 email,
-                password: hashPassword
+                password: hashedpassword
             }
         )
 
         res.status(200).json({ result: newUser })
     } catch (error) {
-        console.log("signup error: " + error.message());
+        console.log("signup error: " + error.message);
         res.status(500).json({ message: "something went wrong ..." })
     }
 }
 
 export async function login(req, res) {
+    console.log("login processed")
     const { email, password } = req.body;
     try {
         const existUser = await users.findOne({ email });
