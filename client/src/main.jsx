5dcb145e8ddfe87c66f3ee6b189a6a7e46bfd248
Devank Singh <adreshsingh271527@gmail.com>
2025-03-17 09:11:24 +0530

    added redux

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore, applyMiddleware, compose } from 'redux';
+import { thunk } from 'redux-thunk'
+
 import App from './App.jsx'
+import reducers from './reducers/index.js';
 import './index.css'
 
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Provider>,
 )
