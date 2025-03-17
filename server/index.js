99ef31a98027bd0927db98b8620492fc705b5c9f
Devank Singh <adreshsingh271527@gmail.com>
2025-03-17 09:21:54 +0530

    added mongodb dependency

--- server/index.js
+++ server/index.js
@@ -1,17 +1,20 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
-import userRoutes from './routes/users';
 
-const PORT = 3000;
+import userRoutes from './routes/users.js';
+
+dotenv.config()
+
 const app = express();
-const DB_URL = "mongodb+srv://admin:admin1234@cluster0.b390x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const PORT = process.env.PORT;
+const DB_URL = process.env.DB_URL;
 
 app.use(express.json({ limit: "300mb", extended: true }))
 app.use(express.urlencoded({ limit: "30mb", extended: true }))
-app.use(cors({ origin: '*' }))
+app.use(cors())
 
 //? user auth
 app.use("/user", userRoutes);
@@ -23,9 +26,13 @@ app.get('/', (req, res) => {
     res.send("Welcome to Backend")
 })
 
-mongoose.connect(DB_URL)
-    .then(() => (app.listen(PORT, () => {
-        console.log(`BACKEND RUNNING ON ${PORT}`)
-    })))
+mongoose.connect(DB_URL, {
+    serverSelectionTimeoutMS: 5000 // Adjust timeout
+})
+    .then(
+        () => (app.listen(PORT, () => {
+            console.log(`BACKEND RUNNING ON ${PORT}`)
+        }))
+    )
     .catch((err) => (console.log(err.message)))
 
